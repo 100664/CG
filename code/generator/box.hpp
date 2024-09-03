7ea00552f3703190bc365812dac1b1c89d4eaf1c
Martim Redondo <116294262+100664@users.noreply.github.com>
2024-09-03 23:37:39 +0100

    Add files via upload

---
+++ code/generator/box.hpp
@@ -0,0 +1,14 @@
+// box.hpp
+#define GL_SILENCE_DEPRECATION
+#ifndef BOX_H
+#define BOX_H
+
+#include "../utils/figure.hpp"
+#include "../utils/point.hpp"
+#include "plane.hpp"
+
+FIGURE generate_box(float length, int divisions);
+
+        
+
+#endif // BOX_H
